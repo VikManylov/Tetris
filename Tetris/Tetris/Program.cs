8cb75443aebae007ec888ac90fc4089ac9e7936c
Viktor <winner1.92@mail.ru>
2022-01-09 23:18:40 +0700

    Переопределил родительский класс Figure как абстрактный, создал абстрактный метод Rotate для поворота фигур, переопредели его 2-х текущих фигурах. Для каждой фигуры свой вариант поворота

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -10,13 +10,20 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Square s = new Square(2, 5, '*');
+            Figure s = new Stick(20, 5, '*');
+
             s.Draw();
 
             Thread.Sleep(500);
 
             s.Hide();
-            s.Move(Direction.LEFT);
+            s.Rotate();
+            s.Draw();
+
+            Thread.Sleep(500);
+
+            s.Hide();
+            s.Move(Direction.DOWN);
             s.Draw();
 
             Thread.Sleep(500);
@@ -28,24 +35,9 @@ namespace Tetris
             Thread.Sleep(500);
 
             s.Hide();
-            s.Move(Direction.DOWN);
+            s.Rotate();
             s.Draw();
-
-           
-
-
             
-
-            //Figure[] f = new Figure[2];
-            //f[0] = new Square(2, 5, '*');
-            //f[1] = new Stick(6, 6, '*');
-
-            //foreach(Figure fig in f)
-            //{
-            //    fig.Draw();
-            //}
-
-
             Console.ReadLine();
         }
 
