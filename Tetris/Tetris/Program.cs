4781cb26d8ead63a3566d27c578d5847643cc5b0
Viktor <winner1.92@mail.ru>
2022-01-18 00:03:55 +0700

    Добавил автоматическое падение фигур с помощью класса Timer. Добавил захват текущей фигуры для исключения "отпадывания" точек от фигуры.

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Threading;
+using System.Timers;
 
 namespace Tetris
 {
     class Program
     {
+        const int TIMER_INTERVAL = 500;
+        static System.Timers.Timer timer;
+        static private Object _lockObject = new object();
+
+        static Figure currentFigure;
         static FigureGenerator generator;
         static void Main(string[] args)
         {
@@ -12,20 +18,23 @@ namespace Tetris
             Console.SetBufferSize(Field.Width, Field.Height);
             
             generator = new FigureGenerator(Field.Width / 2, 0, Drawer.DEFAULT_SYMBOL);
-            Figure currentFigure = generator.GetNewFigure();
+            currentFigure = generator.GetNewFigure();
+            SetTimer();
 
             while(true)
             {
                 if(Console.KeyAvailable)
                 {
                     var key = Console.ReadKey();
+                    Monitor.Enter(_lockObject);
                     var result = HandleKey(currentFigure, key.Key);
-                    Processresult(result, ref currentFigure);
+                    ProcessResult(result, ref currentFigure);
+                    Monitor.Exit(_lockObject);
                 }
             }
         }
 
-        private static bool Processresult(Result result, ref Figure currentFigure)
+        private static bool ProcessResult(Result result, ref Figure currentFigure)
         {
             if (result == Result.HEAP_STRIKE || result == Result.DOWN_BORDER_STRIKE)
             {
@@ -52,6 +61,24 @@ namespace Tetris
                     return currentFigure.TryRotate(); 
             }
             return Result.SUCCESS;
+        }
+
+        private static void SetTimer()
+        {
+            //Create a timer with a two second interval.
+            timer = new System.Timers.Timer(TIMER_INTERVAL);
+            //Hook up the Elapsed event for the timer.
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Monitor.Enter(_lockObject);
+            var result = currentFigure.TryMove(Direction.DOWN);
+            ProcessResult(result, ref currentFigure);
+            Monitor.Exit(_lockObject);
         }
     }
 }
